3d585f3d13650773b446d1f30262e6474d9475f3
Wen100 <mm18069@ues.edu.sv>
2024-06-18 18:29:15 -0600

    MM18069: Arreglo de responsividad en proyecto, agregacion de notificacion de mensaje enviado con exito en el form, y actualización de links a las paginas oficiales de la UES

---
+++ js/contactanos.js
@@ -0,0 +1,8 @@
+document.getElementById('btnEnviar').addEventListener('click', function() {
+    Swal.fire({
+        title: "Enhorabuena!",
+        text: "Formulario enviado con éxito!",
+        icon: "success",
+        timer:  5000,
+    });
+});
